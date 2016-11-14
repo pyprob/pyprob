dfeab2e5b2b8e5f18bd99cb9cec9e021cfea452a
Tuan-Anh Le <tuananh@robots.ox.ac.uk>
2016-11-14 12:27:58 +0000

    fix inconsistencies between torch and clojure

--- compile.lua
+++ compile.lua
@@ -163,7 +163,7 @@ function getBatchTraces(data)
             samples[m][timeStep] = samples[m][timeStep] or {}
             local address = vv['sample-address']
             local instance = vv['sample-instance']
-            local proposalType = vv['proposal-str']
+            local proposalType = vv['proposal-name']
 
             samples[m][timeStep].value = nil
             if type(vv['value']) == 'boolean' then
