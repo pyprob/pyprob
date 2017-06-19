d7ad6494c8222d017e7ab8b2103b9ad376b1b7ef
Rob Cornish <jrmcornish@gmail.com>
2017-06-19 12:19:20 +0100

    Reverted changes (now in `rob` branch)

--- examples/project.clj
+++ examples/project.clj
@@ -5,7 +5,6 @@
                  [org.clojure/tools.cli "0.3.5"]
                  [anglican "1.0.0"]
                  [anglican-infcomp "0.1.0-SNAPSHOT"]
-                 [net.polyc0l0r/clj-hdf5 "0.2.2-SNAPSHOT"]
-                 [rhizome "0.2.7"]]
+                 [net.polyc0l0r/clj-hdf5 "0.2.2-SNAPSHOT"]]
   :java-source-paths ["src/helpers"]
   :main examples.core)
