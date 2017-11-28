6aac747a8d9a55ca91d6408fee9a386289720a8e
Tobias Kohn <webmaster@tobiaskohn.ch>
2017-11-28 17:03:05 +0000

    New scheme for the address of a sample

--- pyprob/state.py
+++ pyprob/state.py
@@ -8,6 +8,7 @@ import pyprob
 from pyprob import util
 from pyprob.trace import Sample, Trace
 import traceback
+import sys
 import torch
 
 current_trace = None
@@ -41,13 +42,59 @@ def end_trace():
     return ret
 
 def extract_address():
-    tb = traceback.extract_stack()
+#    tb = traceback.extract_stack()
     # print()
     # for t in tb:
     #     print(t[0], t[1], t[2], t[3])
-    frame = tb[-3]
+#    frame = tb[-3]
     # return '{0}/{1}/{2}'.format(frame[1], frame[2], frame[3])
-    return '{0}/{1}'.format(frame[1], frame[2])
+#    return '{0}/{1}'.format(frame[1], frame[2])
+    # Retun an address in the format:
+    # 'instruction pointer' / 'qualified function name'
+    frame = sys._getframe(3)
+    ip = frame.f_lasti
+    names = []
+    while frame is not None:
+        names.append(frame.f_code.co_name)
+        frame = frame.f_back
+    return "{}/{}".format(ip, '.'.join(reversed(names)))
+
+
+def extract_target_of_assignment():
+    import opcode
+    frame = sys._getframe(3)
+    code = frame.f_code
+    next_instruction = code.co_code[frame.f_lasti+2]
+    instruction_arg = code.co_code[frame.f_lasti+3]
+    instruction_name = opcode.opname[next_instruction]
+    if instruction_name == 'STORE_FAST':
+        return code.co_varnames[instruction_arg]
+
+    elif instruction_name in ['STORE_NAME', 'STORE_GLOBAL']:
+        return code.co_names[instruction_arg]
+
+    elif instruction_name in ['LOAD_FAST', 'LOAD_NAME', 'LOAD_GLOBAL'] and \
+            opcode.opname[code.co_code[frame.f_lasti+4]] in ['LOAD_CONST', 'LOAD_FAST'] and \
+            opcode.opname[code.co_code[frame.f_lasti+6]] == 'STORE_SUBSCR':
+        base_name = (code.co_varnames if instruction_name == 'LOAD_FAST' else code.co_names)[instruction_arg]
+        second_instruction = opcode.opname[code.co_code[frame.f_lasti+4]]
+        second_arg = code.co_code[frame.f_lasti+5]
+        if second_instruction == 'LOAD_CONST':
+            value = code.co_consts[second_arg]
+        elif second_instruction == 'LOAD_FAST':
+            var_name = code.co_varnames[second_arg]
+            value = frame.f_locals[var_name]
+        else:
+            value = None
+        if type(value) is int:
+            index_name = str(value)
+            return base_name + '[' + index_name + ']'
+        else:
+            return None
+
+    else:
+        return None
+
 
 def sample(distribution):
     global current_trace
