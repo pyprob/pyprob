05f9f5160e7a64bd6258e0666e00708ece0d7f51
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2018-02-16 17:28:18 +0000

    Hidden Markov model test

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -4,14 +4,15 @@ import math
 import torch
 import torch.nn.functional as F
 from termcolor import colored
-import argparse
+import time
 
 import pyprob
 from pyprob import util
 from pyprob import Model
 from pyprob.distributions import Categorical, Empirical, Normal, Uniform
 
-samples = 10000
+
+samples = 5000
 training_traces = 10000
 perf_score_importance_sampling = 0
 perf_score_inference_compilation = 0
@@ -182,48 +183,71 @@ class HiddenMarkovModelTestCase(unittest.TestCase):
                      Normal(1, 1),
                      Normal(0, 1)]
         self._model = HiddenMarkovModel(init_dist, trans_dists, obs_dists)
+
+        self._observation = [0.9, 0.8, 0.7, 0.0, -0.025, -5.0, -2.0, -0.1, 0.0, 0.13, 0.45, 6, 0.2, 0.3, -1, -1]
+        self._posterior_mean_correct = util.to_variable([[0.3775, 0.3092, 0.3133],
+                                                         [0.0416, 0.4045, 0.5539],
+                                                         [0.0541, 0.2552, 0.6907],
+                                                         [0.0455, 0.2301, 0.7244],
+                                                         [0.1062, 0.1217, 0.7721],
+                                                         [0.0714, 0.1732, 0.7554],
+                                                         [0.9300, 0.0001, 0.0699],
+                                                         [0.4577, 0.0452, 0.4971],
+                                                         [0.0926, 0.2169, 0.6905],
+                                                         [0.1014, 0.1359, 0.7626],
+                                                         [0.0985, 0.1575, 0.7440],
+                                                         [0.1781, 0.2198, 0.6022],
+                                                         [0.0000, 0.9848, 0.0152],
+                                                         [0.1130, 0.1674, 0.7195],
+                                                         [0.0557, 0.1848, 0.7595],
+                                                         [0.2017, 0.0472, 0.7511],
+                                                         [0.2545, 0.0611, 0.6844]])
         super().__init__(*args, **kwargs)
 
     def test_inference_hmm_posterior_importance_sampling(self):
-        observation = [0.9, 0.8, 0.7, 0.0, -0.025, -5.0, -2.0, -0.1, 0.0, 0.13, 0.45, 6, 0.2, 0.3, -1, -1]
-        posterior_mean_correct = util.to_variable([[0.3775, 0.3092, 0.3133],
-                                                   [0.0416, 0.4045, 0.5539],
-                                                   [0.0541, 0.2552, 0.6907],
-                                                   [0.0455, 0.2301, 0.7244],
-                                                   [0.1062, 0.1217, 0.7721],
-                                                   [0.0714, 0.1732, 0.7554],
-                                                   [0.9300, 0.0001, 0.0699],
-                                                   [0.4577, 0.0452, 0.4971],
-                                                   [0.0926, 0.2169, 0.6905],
-                                                   [0.1014, 0.1359, 0.7626],
-                                                   [0.0985, 0.1575, 0.7440],
-                                                   [0.1781, 0.2198, 0.6022],
-                                                   [0.0000, 0.9848, 0.0152],
-                                                   [0.1130, 0.1674, 0.7195],
-                                                   [0.0557, 0.1848, 0.7595],
-                                                   [0.2017, 0.0472, 0.7511],
-                                                   [0.2545, 0.0611, 0.6844]])
+        observation = self._observation
+        posterior_mean_correct = self._posterior_mean_correct
 
         posterior = self._model.posterior_distribution(samples, observation=observation)
-        posterior_mean = posterior.mean
         posterior_mean_unweighted = posterior.mean_unweighted
+        posterior_mean = posterior.mean
+
+        l2_distance = float(F.pairwise_distance(posterior_mean, posterior_mean_correct).sum())
+
+        util.debug('samples', 'posterior_mean_unweighted', 'posterior_mean', 'posterior_mean_correct', 'l2_distance')
+
+        self.assertLess(l2_distance, 3)
+        add_perf_score_importance_sampling(l2_distance)
+
+    def test_inference_hmm_posterior_inference_compilation(self):
+        observation = self._observation
+        posterior_mean_correct = self._posterior_mean_correct
+
+        self._model.learn_inference_network(observation=torch.zeros(16,3), early_stop_traces=training_traces, learning_rate=0.0001)
+        posterior = self._model.posterior_distribution(samples, use_inference_network=True, observation=observation)
+        posterior_mean_unweighted = posterior.mean_unweighted
+        posterior_mean = posterior.mean
+
         l2_distance = float(F.pairwise_distance(posterior_mean, posterior_mean_correct).sum())
 
         util.debug('samples', 'posterior_mean_unweighted', 'posterior_mean', 'posterior_mean_correct', 'l2_distance')
 
         self.assertLess(l2_distance, 2)
+        add_perf_score_inference_compilation(l2_distance)
 
 
 if __name__ == '__main__':
     if torch.cuda.is_available():
         pyprob.set_cuda(True)
     tests = []
-    tests.append('GaussianWithUnknownMeanTestCase')
-    tests.append('GaussianWithUnknownMeanMarsagliaTestCase')
-    # tests.append('HiddenMarkovModelTestCase')
+    # tests.append('GaussianWithUnknownMeanTestCase')
+    # tests.append('GaussianWithUnknownMeanMarsagliaTestCase')
+    tests.append('HiddenMarkovModelTestCase')
 
+    time_start = time.time()
     success = unittest.main(defaultTest=tests, verbosity=2, exit=False).result.wasSuccessful()
-    print('\nModels run           : {}'.format(' '.join(tests)))
+    print('\nDuration             : {}'.format(util.days_hours_mins_secs_str(time.time() - time_start)))
+    print('Models run           : {}'.format(' '.join(tests)))
     print('Samples              : {}'.format(samples))
     print('Training traces      : {}\n'.format(training_traces))
     print('\nTotal inference performance scores\n')
