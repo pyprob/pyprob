76fc2b57c4d8b132b14dc8f37b5f1a1fc183eb64
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2018-03-22 16:23:46 +0000

    Add 2D convnet

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -4,11 +4,32 @@ from torch.autograd import Variable
 
 import pyprob
 from pyprob import util
-from pyprob.nn import ObserveEmbeddingConvnet3D4C
+from pyprob.nn import ObserveEmbeddingConvNet2D5C, ObserveEmbeddingConvNet3D4C
 
 
 class NNTestCase(unittest.TestCase):
-    def test_ObserveEmbeddingConvnet3D4C(self):
+    def test_ObserveEmbeddingConvNet2D5C(self):
+        batch_size = 32
+        channels = 3
+        output_dim = 128
+        input_batch_shape = [batch_size, channels, 20, 20]
+        output_batch_shape_correct = [batch_size, output_dim]
+        input_non_batch_shape = [channels, 20, 20]
+        output_non_batch_shape_correct = [1, output_dim]
+
+        input_batch = Variable(torch.Tensor(torch.Size(input_batch_shape)))
+        input_non_batch = Variable(torch.Tensor(torch.Size(input_non_batch_shape)))
+        nn = ObserveEmbeddingConvNet2D5C(input_example_non_batch=input_non_batch, output_dim=output_dim)
+        nn.configure()
+        output_batch_shape = list(nn.forward(input_batch).size())
+        output_non_batch_shape = list(nn.forward(input_non_batch.unsqueeze(0)).size())
+
+        util.debug('batch_size', 'channels', 'output_dim', 'input_batch_shape', 'output_batch_shape', 'output_batch_shape_correct', 'input_non_batch_shape', 'output_non_batch_shape', 'output_non_batch_shape_correct')
+
+        self.assertEqual(output_batch_shape, output_batch_shape_correct)
+        self.assertEqual(output_non_batch_shape, output_non_batch_shape_correct)
+
+    def test_ObserveEmbeddingConvNet3D4C(self):
         batch_size = 32
         channels = 3
         output_dim = 128
@@ -19,7 +40,7 @@ class NNTestCase(unittest.TestCase):
 
         input_batch = Variable(torch.Tensor(torch.Size(input_batch_shape)))
         input_non_batch = Variable(torch.Tensor(torch.Size(input_non_batch_shape)))
-        nn = ObserveEmbeddingConvnet3D4C(input_example_non_batch=input_non_batch, output_dim=output_dim)
+        nn = ObserveEmbeddingConvNet3D4C(input_example_non_batch=input_non_batch, output_dim=output_dim)
         nn.configure()
         output_batch_shape = list(nn.forward(input_batch).size())
         output_non_batch_shape = list(nn.forward(input_non_batch.unsqueeze(0)).size())
@@ -28,18 +49,6 @@ class NNTestCase(unittest.TestCase):
 
         self.assertEqual(output_batch_shape, output_batch_shape_correct)
         self.assertEqual(output_non_batch_shape, output_non_batch_shape_correct)
-
-        # samples = 5000
-        # prior_mean_correct = 1
-        # prior_stddev_correct = math.sqrt(5)
-        #
-        # prior = self._model.prior_distribution(samples)
-        # prior_mean = float(prior.mean)
-        # prior_stddev = float(prior.stddev)
-        # util.debug('samples', 'prior_mean', 'prior_mean_correct', 'prior_stddev', 'prior_stddev_correct')
-        #
-        # self.assertAlmostEqual(prior_mean, prior_mean_correct, places=0)
-        # self.assertAlmostEqual(prior_stddev, prior_stddev_correct, places=0)
 
 
 if __name__ == '__main__':
