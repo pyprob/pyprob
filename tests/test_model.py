351da5f8abe2a5cf313c186df7df2cf5d82bd463
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2018-09-09 23:32:43 +0100

    Model tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -147,146 +147,192 @@ class ModelWithReplacementTestCase(unittest.TestCase):
         self.assertAlmostEqual(trace_length_stddev, trace_length_stddev_correct, places=0)
         self.assertAlmostEqual(trace_length_min, trace_length_min_correct, places=0)
         self.assertAlmostEqual(trace_length_max, trace_length_max_correct, places=0)
-#
-#
-# class ModelObservationStyle1TestCase(unittest.TestCase):
-#     def __init__(self, *args, **kwargs):
-#         # http://www.robots.ox.ac.uk/~fwood/assets/pdf/Wood-AISTATS-2014.pdf
-#         class GaussianWithUnknownMean(Model):
-#             def __init__(self, prior_mean=1, prior_stddev=math.sqrt(5), likelihood_stddev=math.sqrt(2)):
-#                 self.prior_mean = prior_mean
-#                 self.prior_stddev = prior_stddev
-#                 self.likelihood_stddev = likelihood_stddev
-#                 super().__init__('Gaussian with unknown mean')
-#
-#             def forward(self):
-#                 mu = pyprob.sample(Normal(self.prior_mean, self.prior_stddev))
-#                 likelihood = Normal(mu, self.likelihood_stddev)
-#                 # pyprob.observe usage alternative #1
-#                 observation = pyprob.observe(name='obs')
-#                 for o in observation:
-#                     pyprob.observe(likelihood, o)
-#                 return mu
-#
-#         self._model = GaussianWithUnknownMean()
-#         super().__init__(*args, **kwargs)
-#
-#     def test_observation_style1_gum_posterior_importance_sampling(self):
-#         samples = importance_sampling_samples
-#         true_posterior = Normal(7.25, math.sqrt(1/1.2))
-#         posterior_mean_correct = float(true_posterior.mean)
-#         posterior_stddev_correct = float(true_posterior.stddev)
-#         prior_mean_correct = 1.
-#         prior_stddev_correct = math.sqrt(5)
-#
-#         posterior = self._model.posterior_distribution(samples, inference_engine=InferenceEngine.IMPORTANCE_SAMPLING, observe={'obs': [8, 9]})
-#
-#         posterior_mean = float(posterior.mean)
-#         posterior_mean_unweighted = float(posterior.unweighted().mean)
-#         posterior_stddev = float(posterior.stddev)
-#         posterior_stddev_unweighted = float(posterior.unweighted().stddev)
-#         kl_divergence = float(pyprob.distributions.Distribution.kl_divergence(true_posterior, Normal(posterior.mean, posterior.stddev)))
-#
-#         util.debug('samples', 'prior_mean_correct', 'posterior_mean_unweighted', 'posterior_mean', 'posterior_mean_correct', 'prior_stddev_correct', 'posterior_stddev_unweighted', 'posterior_stddev', 'posterior_stddev_correct', 'kl_divergence')
-#
-#         self.assertAlmostEqual(posterior_mean_unweighted, prior_mean_correct, places=0)
-#         self.assertAlmostEqual(posterior_stddev_unweighted, prior_stddev_correct, places=0)
-#         self.assertAlmostEqual(posterior_mean, posterior_mean_correct, places=0)
-#         self.assertAlmostEqual(posterior_stddev, posterior_stddev_correct, places=0)
-#         self.assertLess(kl_divergence, 0.25)
-#
-#
-# class ModelObservationStyle2TestCase(unittest.TestCase):
-#     def __init__(self, *args, **kwargs):
-#         # http://www.robots.ox.ac.uk/~fwood/assets/pdf/Wood-AISTATS-2014.pdf
-#         class GaussianWithUnknownMean(Model):
-#             def __init__(self, prior_mean=1, prior_stddev=math.sqrt(5), likelihood_stddev=math.sqrt(2)):
-#                 self.prior_mean = prior_mean
-#                 self.prior_stddev = prior_stddev
-#                 self.likelihood_stddev = likelihood_stddev
-#                 super().__init__('Gaussian with unknown mean')
-#
-#             def forward(self):
-#                 mu = pyprob.sample(Normal(self.prior_mean, self.prior_stddev))
-#                 likelihood = Normal(mu, self.likelihood_stddev)
-#                 # pyprob.observe usage alternative #2
-#                 pyprob.observe(likelihood, 0, name='obs1')
-#                 pyprob.observe(likelihood, 0, name='obs2')
-#                 return mu
-#
-#         self._model = GaussianWithUnknownMean()
-#         super().__init__(*args, **kwargs)
-#
-#     def test_observation_style2_gum_posterior_importance_sampling(self):
-#         samples = importance_sampling_samples
-#         true_posterior = Normal(7.25, math.sqrt(1/1.2))
-#         posterior_mean_correct = float(true_posterior.mean)
-#         posterior_stddev_correct = float(true_posterior.stddev)
-#         prior_mean_correct = 1.
-#         prior_stddev_correct = math.sqrt(5)
-#
-#         posterior = self._model.posterior_distribution(samples, inference_engine=InferenceEngine.IMPORTANCE_SAMPLING, observe={'obs1': 8, 'obs2': 9})
-#
-#         posterior_mean = float(posterior.mean)
-#         posterior_mean_unweighted = float(posterior.unweighted().mean)
-#         posterior_stddev = float(posterior.stddev)
-#         posterior_stddev_unweighted = float(posterior.unweighted().stddev)
-#         kl_divergence = float(pyprob.distributions.Distribution.kl_divergence(true_posterior, Normal(posterior.mean, posterior.stddev)))
-#
-#         util.debug('samples', 'prior_mean_correct', 'posterior_mean_unweighted', 'posterior_mean', 'posterior_mean_correct', 'prior_stddev_correct', 'posterior_stddev_unweighted', 'posterior_stddev', 'posterior_stddev_correct', 'kl_divergence')
-#
-#         self.assertAlmostEqual(posterior_mean_unweighted, prior_mean_correct, places=0)
-#         self.assertAlmostEqual(posterior_stddev_unweighted, prior_stddev_correct, places=0)
-#         self.assertAlmostEqual(posterior_mean, posterior_mean_correct, places=0)
-#         self.assertAlmostEqual(posterior_stddev, posterior_stddev_correct, places=0)
-#         self.assertLess(kl_divergence, 0.25)
-#
-#
-# class ModelObservationStyle3TestCase(unittest.TestCase):
-#     def __init__(self, *args, **kwargs):
-#         # http://www.robots.ox.ac.uk/~fwood/assets/pdf/Wood-AISTATS-2014.pdf
-#         class GaussianWithUnknownMean(Model):
-#             def __init__(self, prior_mean=1, prior_stddev=math.sqrt(5), likelihood_stddev=math.sqrt(2)):
-#                 self.prior_mean = prior_mean
-#                 self.prior_stddev = prior_stddev
-#                 self.likelihood_stddev = likelihood_stddev
-#                 super().__init__('Gaussian with unknown mean')
-#
-#             def forward(self):
-#                 mu = pyprob.sample(Normal(self.prior_mean, self.prior_stddev))
-#                 likelihood = Normal(mu, self.likelihood_stddev)
-#                 # pyprob.observe usage alternative #3
-#                 pyprob.observe(likelihood, name='obs1')
-#                 pyprob.observe(likelihood, name='obs2')
-#                 return mu
-#
-#         self._model = GaussianWithUnknownMean()
-#         super().__init__(*args, **kwargs)
-#
-#     def test_observation_style3_gum_posterior_importance_sampling(self):
-#         samples = importance_sampling_samples
-#         true_posterior = Normal(7.25, math.sqrt(1/1.2))
-#         posterior_mean_correct = float(true_posterior.mean)
-#         posterior_stddev_correct = float(true_posterior.stddev)
-#         prior_mean_correct = 1.
-#         prior_stddev_correct = math.sqrt(5)
-#
-#         posterior = self._model.posterior_distribution(samples, inference_engine=InferenceEngine.IMPORTANCE_SAMPLING, observe={'obs1': 8, 'obs2': 9})
-#
-#         posterior_mean = float(posterior.mean)
-#         posterior_mean_unweighted = float(posterior.unweighted().mean)
-#         posterior_stddev = float(posterior.stddev)
-#         posterior_stddev_unweighted = float(posterior.unweighted().stddev)
-#         kl_divergence = float(pyprob.distributions.Distribution.kl_divergence(true_posterior, Normal(posterior.mean, posterior.stddev)))
-#
-#         util.debug('samples', 'prior_mean_correct', 'posterior_mean_unweighted', 'posterior_mean', 'posterior_mean_correct', 'prior_stddev_correct', 'posterior_stddev_unweighted', 'posterior_stddev', 'posterior_stddev_correct', 'kl_divergence')
-#
-#         self.assertAlmostEqual(posterior_mean_unweighted, prior_mean_correct, places=0)
-#         self.assertAlmostEqual(posterior_stddev_unweighted, prior_stddev_correct, places=0)
-#         self.assertAlmostEqual(posterior_mean, posterior_mean_correct, places=0)
-#         self.assertAlmostEqual(posterior_stddev, posterior_stddev_correct, places=0)
-#         self.assertLess(kl_divergence, 0.25)
-#
+
+
+class ModelObservationStyle1TestCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        # http://www.robots.ox.ac.uk/~fwood/assets/pdf/Wood-AISTATS-2014.pdf
+        class GaussianWithUnknownMean(Model):
+            def __init__(self, prior_mean=1, prior_stddev=math.sqrt(5), likelihood_stddev=math.sqrt(2)):
+                self.prior_mean = prior_mean
+                self.prior_stddev = prior_stddev
+                self.likelihood_stddev = likelihood_stddev
+                super().__init__('Gaussian with unknown mean')
+
+            def forward(self):
+                mu = pyprob.sample(Normal(self.prior_mean, self.prior_stddev))
+                likelihood = Normal(mu, self.likelihood_stddev)
+                # pyprob.observe usage alternative #1
+                observation = pyprob.observe(name='obs')
+                for o in observation:
+                    pyprob.observe(likelihood, o)
+                return mu
+
+        self._model = GaussianWithUnknownMean()
+        super().__init__(*args, **kwargs)
+
+    def test_observation_style1_gum_posterior_importance_sampling(self):
+        samples = importance_sampling_samples
+        true_posterior = Normal(7.25, math.sqrt(1/1.2))
+        posterior_mean_correct = float(true_posterior.mean)
+        posterior_stddev_correct = float(true_posterior.stddev)
+        prior_mean_correct = 1.
+        prior_stddev_correct = math.sqrt(5)
+
+        posterior = self._model.posterior_distribution(samples, inference_engine=InferenceEngine.IMPORTANCE_SAMPLING, observe={'obs': [8, 9]})
+
+        posterior_mean = float(posterior.mean)
+        posterior_mean_unweighted = float(posterior.unweighted().mean)
+        posterior_stddev = float(posterior.stddev)
+        posterior_stddev_unweighted = float(posterior.unweighted().stddev)
+        kl_divergence = float(pyprob.distributions.Distribution.kl_divergence(true_posterior, Normal(posterior.mean, posterior.stddev)))
+
+        util.debug('samples', 'prior_mean_correct', 'posterior_mean_unweighted', 'posterior_mean', 'posterior_mean_correct', 'prior_stddev_correct', 'posterior_stddev_unweighted', 'posterior_stddev', 'posterior_stddev_correct', 'kl_divergence')
+
+        self.assertAlmostEqual(posterior_mean_unweighted, prior_mean_correct, places=0)
+        self.assertAlmostEqual(posterior_stddev_unweighted, prior_stddev_correct, places=0)
+        self.assertAlmostEqual(posterior_mean, posterior_mean_correct, places=0)
+        self.assertAlmostEqual(posterior_stddev, posterior_stddev_correct, places=0)
+        self.assertLess(kl_divergence, 0.25)
+
+
+class ModelObservationStyle2TestCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        # http://www.robots.ox.ac.uk/~fwood/assets/pdf/Wood-AISTATS-2014.pdf
+        class GaussianWithUnknownMean(Model):
+            def __init__(self, prior_mean=1, prior_stddev=math.sqrt(5), likelihood_stddev=math.sqrt(2)):
+                self.prior_mean = prior_mean
+                self.prior_stddev = prior_stddev
+                self.likelihood_stddev = likelihood_stddev
+                super().__init__('Gaussian with unknown mean')
+
+            def forward(self):
+                mu = pyprob.sample(Normal(self.prior_mean, self.prior_stddev))
+                likelihood = Normal(mu, self.likelihood_stddev)
+                # pyprob.observe usage alternative #2
+                pyprob.observe(likelihood, 0, name='obs1')
+                pyprob.observe(likelihood, 0, name='obs2')
+                return mu
+
+        self._model = GaussianWithUnknownMean()
+        super().__init__(*args, **kwargs)
+
+    def test_observation_style2_gum_posterior_importance_sampling(self):
+        samples = importance_sampling_samples
+        true_posterior = Normal(7.25, math.sqrt(1/1.2))
+        posterior_mean_correct = float(true_posterior.mean)
+        posterior_stddev_correct = float(true_posterior.stddev)
+        prior_mean_correct = 1.
+        prior_stddev_correct = math.sqrt(5)
+
+        posterior = self._model.posterior_distribution(samples, inference_engine=InferenceEngine.IMPORTANCE_SAMPLING, observe={'obs1': 8, 'obs2': 9})
+
+        posterior_mean = float(posterior.mean)
+        posterior_mean_unweighted = float(posterior.unweighted().mean)
+        posterior_stddev = float(posterior.stddev)
+        posterior_stddev_unweighted = float(posterior.unweighted().stddev)
+        kl_divergence = float(pyprob.distributions.Distribution.kl_divergence(true_posterior, Normal(posterior.mean, posterior.stddev)))
+
+        util.debug('samples', 'prior_mean_correct', 'posterior_mean_unweighted', 'posterior_mean', 'posterior_mean_correct', 'prior_stddev_correct', 'posterior_stddev_unweighted', 'posterior_stddev', 'posterior_stddev_correct', 'kl_divergence')
+
+        self.assertAlmostEqual(posterior_mean_unweighted, prior_mean_correct, places=0)
+        self.assertAlmostEqual(posterior_stddev_unweighted, prior_stddev_correct, places=0)
+        self.assertAlmostEqual(posterior_mean, posterior_mean_correct, places=0)
+        self.assertAlmostEqual(posterior_stddev, posterior_stddev_correct, places=0)
+        self.assertLess(kl_divergence, 0.25)
+
+
+class ModelObservationStyle3TestCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        # http://www.robots.ox.ac.uk/~fwood/assets/pdf/Wood-AISTATS-2014.pdf
+        class GaussianWithUnknownMean(Model):
+            def __init__(self, prior_mean=1, prior_stddev=math.sqrt(5), likelihood_stddev=math.sqrt(2)):
+                self.prior_mean = prior_mean
+                self.prior_stddev = prior_stddev
+                self.likelihood_stddev = likelihood_stddev
+                super().__init__('Gaussian with unknown mean')
+
+            def forward(self):
+                mu = pyprob.sample(Normal(self.prior_mean, self.prior_stddev))
+                likelihood = Normal(mu, self.likelihood_stddev)
+                # pyprob.observe usage alternative #3
+                pyprob.observe(likelihood, name='obs1')
+                pyprob.observe(likelihood, name='obs2')
+                return mu
+
+        self._model = GaussianWithUnknownMean()
+        super().__init__(*args, **kwargs)
+
+    def test_observation_style3_gum_posterior_importance_sampling(self):
+        samples = importance_sampling_samples
+        true_posterior = Normal(7.25, math.sqrt(1/1.2))
+        posterior_mean_correct = float(true_posterior.mean)
+        posterior_stddev_correct = float(true_posterior.stddev)
+        prior_mean_correct = 1.
+        prior_stddev_correct = math.sqrt(5)
+
+        posterior = self._model.posterior_distribution(samples, inference_engine=InferenceEngine.IMPORTANCE_SAMPLING, observe={'obs1': 8, 'obs2': 9})
+
+        posterior_mean = float(posterior.mean)
+        posterior_mean_unweighted = float(posterior.unweighted().mean)
+        posterior_stddev = float(posterior.stddev)
+        posterior_stddev_unweighted = float(posterior.unweighted().stddev)
+        kl_divergence = float(pyprob.distributions.Distribution.kl_divergence(true_posterior, Normal(posterior.mean, posterior.stddev)))
+
+        util.debug('samples', 'prior_mean_correct', 'posterior_mean_unweighted', 'posterior_mean', 'posterior_mean_correct', 'prior_stddev_correct', 'posterior_stddev_unweighted', 'posterior_stddev', 'posterior_stddev_correct', 'kl_divergence')
+
+        self.assertAlmostEqual(posterior_mean_unweighted, prior_mean_correct, places=0)
+        self.assertAlmostEqual(posterior_stddev_unweighted, prior_stddev_correct, places=0)
+        self.assertAlmostEqual(posterior_mean, posterior_mean_correct, places=0)
+        self.assertAlmostEqual(posterior_stddev, posterior_stddev_correct, places=0)
+        self.assertLess(kl_divergence, 0.25)
+
+
+class ModelObservationStyle4TestCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        # http://www.robots.ox.ac.uk/~fwood/assets/pdf/Wood-AISTATS-2014.pdf
+        class GaussianWithUnknownMean(Model):
+            def __init__(self, prior_mean=1, prior_stddev=math.sqrt(5), likelihood_stddev=math.sqrt(2)):
+                self.prior_mean = prior_mean
+                self.prior_stddev = prior_stddev
+                self.likelihood_stddev = likelihood_stddev
+                super().__init__('Gaussian with unknown mean')
+
+            def forward(self):
+                mu = pyprob.sample(Normal(self.prior_mean, self.prior_stddev))
+                likelihood = Normal(mu, self.likelihood_stddev)
+                # pyprob.observe usage alternative #4
+                pyprob.sample(likelihood, name='obs1')
+                pyprob.sample(likelihood, name='obs2')
+                return mu
+
+        self._model = GaussianWithUnknownMean()
+        super().__init__(*args, **kwargs)
+
+    def test_observation_style4_gum_posterior_importance_sampling(self):
+        samples = importance_sampling_samples
+        true_posterior = Normal(7.25, math.sqrt(1/1.2))
+        posterior_mean_correct = float(true_posterior.mean)
+        posterior_stddev_correct = float(true_posterior.stddev)
+        prior_mean_correct = 1.
+        prior_stddev_correct = math.sqrt(5)
+
+        posterior = self._model.posterior_distribution(samples, inference_engine=InferenceEngine.IMPORTANCE_SAMPLING, observe={'obs1': 8, 'obs2': 9})
+
+        posterior_mean = float(posterior.mean)
+        posterior_mean_unweighted = float(posterior.unweighted().mean)
+        posterior_stddev = float(posterior.stddev)
+        posterior_stddev_unweighted = float(posterior.unweighted().stddev)
+        kl_divergence = float(pyprob.distributions.Distribution.kl_divergence(true_posterior, Normal(posterior.mean, posterior.stddev)))
+
+        util.debug('samples', 'prior_mean_correct', 'posterior_mean_unweighted', 'posterior_mean', 'posterior_mean_correct', 'prior_stddev_correct', 'posterior_stddev_unweighted', 'posterior_stddev', 'posterior_stddev_correct', 'kl_divergence')
+
+        self.assertAlmostEqual(posterior_mean_unweighted, prior_mean_correct, places=0)
+        self.assertAlmostEqual(posterior_stddev_unweighted, prior_stddev_correct, places=0)
+        self.assertAlmostEqual(posterior_mean, posterior_mean_correct, places=0)
+        self.assertAlmostEqual(posterior_stddev, posterior_stddev_correct, places=0)
+        self.assertLess(kl_divergence, 0.25)
+
 
 if __name__ == '__main__':
     pyprob.set_verbosity(2)
