a0eb2ec03b241007e86107575489d117d7bd3aba
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2018-09-10 06:16:54 +0100

    Analytics complete

--- pyprob/analytics.py
+++ pyprob/analytics.py
@@ -13,18 +13,18 @@ class Analytics():
     def __init__(self, model):
         self._model = model
 
-    def prior_statistics(self, num_traces=1000, prior_inflation=PriorInflation.DISABLED, controlled_only=False, bins=100, log_xscale=False, log_yscale=False, n_most_frequent=None, file_name=None, *args, **kwargs):
+    def prior_statistics(self, num_traces=1000, prior_inflation=PriorInflation.DISABLED, controlled_only=False, use_address_base=True, bins=100, log_xscale=False, log_yscale=False, n_most_frequent=None, file_name=None, *args, **kwargs):
         trace_dist = self._model.prior_traces(num_traces=num_traces, prior_inflation=prior_inflation, *args, **kwargs)
-        return self._collect_statistics(trace_dist, controlled_only, n_most_frequent, file_name, bins, log_xscale, log_yscale)
+        return self._collect_statistics(trace_dist, controlled_only, use_address_base, n_most_frequent, file_name, bins, log_xscale, log_yscale)
 
-    def posterior_statistics(self, num_traces=1000, inference_engine=InferenceEngine.IMPORTANCE_SAMPLING, observe=None, controlled_only=False, bins=100, log_xscale=False, log_yscale=False, n_most_frequent=None, file_name=None, *args, **kwargs):
+    def posterior_statistics(self, num_traces=1000, inference_engine=InferenceEngine.IMPORTANCE_SAMPLING, observe=None, controlled_only=False, use_address_base=True, bins=100, log_xscale=False, log_yscale=False, n_most_frequent=None, file_name=None, *args, **kwargs):
         trace_dist = self._model.posterior_traces(num_traces=num_traces, inference_engine=inference_engine, observe=observe, *args, **kwargs)
-        return self._collect_statistics(trace_dist, controlled_only, n_most_frequent, file_name, bins, log_xscale, log_yscale)
+        return self._collect_statistics(trace_dist, controlled_only, use_address_base, n_most_frequent, file_name, bins, log_xscale, log_yscale)
 
-    def trace_distribution_statistics(self, trace_dist, controlled_only=False, bins=100, log_xscale=False, log_yscale=False, n_most_frequent=None, file_name=None):
-        return self._collect_statistics(trace_dist, controlled_only, n_most_frequent, file_name, bins, log_xscale, log_yscale)
+    def trace_distribution_statistics(self, trace_dist, controlled_only=False, use_address_base=True, bins=100, log_xscale=False, log_yscale=False, n_most_frequent=None, file_name=None):
+        return self._collect_statistics(trace_dist, controlled_only, use_address_base, n_most_frequent, file_name, bins, log_xscale, log_yscale)
 
-    def _collect_statistics(self, trace_dist, controlled_only, n_most_frequent, file_name, bins, log_xscale, log_yscale):
+    def _collect_statistics(self, trace_dist, controlled_only, use_address_base, n_most_frequent, file_name, bins, log_xscale, log_yscale):
         if controlled_only:
             trace_length_dist = trace_dist.map(lambda trace: len(trace.variables_controlled))
         else:
@@ -40,7 +40,7 @@ class Analytics():
         address_id_to_color = {}
         for trace in traces:
             for variable in trace.variables:
-                address = variable.address_base
+                address = variable.address_base if use_address_base else variable.address
                 if address not in address_stats:
                     address_id = 'A' + str(len(address_stats) + 1)
                     if variable.control:
@@ -61,10 +61,10 @@ class Analytics():
 
         trace_stats = OrderedDict()
         for trace in traces:
-            trace_str = ''.join([variable.address_base for variable in trace.variables])
+            trace_str = ''.join([variable.address_base if use_address_base else variable.address for variable in trace.variables])
             if trace_str not in trace_stats:
                 trace_id = 'T' + str(len(trace_stats) + 1)
-                address_id_sequence = [address_stats[variable.address_base][1] for variable in trace.variables]
+                address_id_sequence = [address_stats[variable.address_base if use_address_base else variable.address][1] for variable in trace.variables]
                 trace_stats[trace_str] = [1, trace_id, trace, address_id_sequence]
             else:
                 trace_stats[trace_str][0] += 1
@@ -111,32 +111,33 @@ class Analytics():
         stats['trace_execution_time_mean'] = float(trace_execution_time_dist.mean)
         stats['trace_execution_time_stddev'] = float(trace_execution_time_dist.stddev)
 
-        print('Generating latent structure graphs...')
+        print('Generating latent structure master graph...')
         sorted_trace_stats = OrderedDict(sorted(dict(trace_stats).items(), key=lambda x: x[1][0], reverse=True))
-        if n_most_frequent is not None:
-            sorted_trace_stats = dict(islice(sorted_trace_stats.items(), n_most_frequent))
+        if n_most_frequent is None:
+            n_most_frequent = len(trace_stats)
+        sorted_trace_stats = dict(islice(sorted_trace_stats.items(), n_most_frequent))
 
-        master_trace_nodes = {}
-        master_trace_edges = {}
+        master_graph_nodes = {}
+        master_graph_edges = {}
         transition_count_total = 0
         for key, value in sorted_trace_stats.items():
             count = value[0]
             address_id_sequence = value[3]
             for address_id in address_id_sequence:
-                if address_id in master_trace_nodes:
-                    master_trace_nodes[address_id] += count
+                if address_id in master_graph_nodes:
+                    master_graph_nodes[address_id] += count
                 else:
-                    master_trace_nodes[address_id] = count
+                    master_graph_nodes[address_id] = count
             for left, right in zip(address_id_sequence, address_id_sequence[1:]):
-                if (left, right) in master_trace_edges:
-                    master_trace_edges[(left, right)] += count
+                if (left, right) in master_graph_edges:
+                    master_graph_edges[(left, right)] += count
                 else:
-                    master_trace_edges[(left, right)] = count
+                    master_graph_edges[(left, right)] = count
                 transition_count_total += count
 
         master_graph = pydotplus.graphviz.Dot(graph_type='digraph', rankdir='LR')
         transition_count_max = 0
-        for edge, count in master_trace_edges.items():
+        for edge, count in master_graph_edges.items():
             if count > transition_count_max:
                 transition_count_max = count
             nodes = master_graph.get_node(edge[0])
@@ -152,11 +153,11 @@ class Analytics():
                 n1 = pydotplus.Node(edge[1])
                 master_graph.add_node(n1)
             master_graph.add_edge(pydotplus.Edge(n0, n1, weight=count))
-            # for node in master_graph.get_nodes():
-            #     node.set_color('gray')
-            #     node.set_fontcolor('gray')
-            # for edge in master_graph.get_edges():
-            #     edge.set_color('gray')
+            for node in master_graph.get_nodes():
+                node.set_color('gray')
+                node.set_fontcolor('gray')
+            for edge in master_graph.get_edges():
+                edge.set_color('gray')
 
         master_graph_annotated = pydotplus.graphviz.graph_from_dot_data(master_graph.to_string())
         for node in master_graph_annotated.get_nodes():
@@ -166,22 +167,93 @@ class Analytics():
             node.set_fillcolor(address_id_to_color[address_id])
             node.set_color('black')
             node.set_fontcolor('black')
-            count = master_trace_nodes[address_id]
+            count = master_graph_nodes[address_id]
             color_factor = 0.75 * (math.exp(1. - count / transition_count_max) - 1.) / (math.e - 1.)
             # color = rgb_to_hex((color_factor, color_factor, color_factor))
             # node.set_color(color)
             node.set_penwidth(max(0.1, 4 * (1 - color_factor)))
         for edge in master_graph_annotated.get_edges():
             (left, right) = edge.obj_dict['points']
-            count = master_trace_edges[(left, right)]
+            count = master_graph_edges[(left, right)]
             edge.set_label('\"{:,.6f}\"'.format(count / transition_count_max))
             # color_factor = 1 - max(0.15, min(1, count/transition_count_max))
             color_factor = 0.75 * (math.exp(1. - count / transition_count_max) - 1.) / (math.e - 1.)
             color = util.rgb_to_hex((color_factor, color_factor, color_factor))
-        #     edge.set_color('black')
+            # edge.set_color('black')
             edge.set_color(color)
             # edge.set_penwidth(max(0.1, 5 * count / transition_count_max))
 
+        graphs = []
+        i = 0
+        for key, value in sorted_trace_stats.items():
+            i += 1
+            count = value[0]
+            address_id_sequence = value[3]
+            print('Generating latent structure trace graph {} of {}...'.format(i, n_most_frequent))
+
+            graph_nodes = {}
+            graph_edges = {}
+            transition_count_total = 0
+            # for key, value in sorted_trace_stats.items():
+            #     count = value[0]
+            #     address_id_sequence = value[3]
+            for address_id in address_id_sequence:
+                if address_id in graph_nodes:
+                    graph_nodes[address_id] += count
+                else:
+                    graph_nodes[address_id] = count
+            for left, right in zip(address_id_sequence, address_id_sequence[1:]):
+                if (left, right) in graph_edges:
+                    graph_edges[(left, right)] += count
+                else:
+                    graph_edges[(left, right)] = count
+                transition_count_total += count
+
+            graph = pydotplus.graphviz.graph_from_dot_data(master_graph.to_string())
+            transition_count_max = 0
+            for edge, count in graph_edges.items():
+                if count > transition_count_max:
+                    transition_count_max = count
+                # nodes = graph.get_node(edge[0])
+                # if len(nodes) > 0:
+                #     n0 = nodes[0]
+                # else:
+                #     n0 = pydotplus.Node(edge[0])
+                #     graph.add_node(n0)
+                # nodes = graph.get_node(edge[1])
+                # if len(nodes) > 0:
+                #     n1 = nodes[0]
+                # else:
+                #     n1 = pydotplus.Node(edge[1])
+                #     graph.add_node(n1)
+                # graph.add_edge(pydotplus.Edge(n0, n1, weight=count))
+
+            for node in graph.get_nodes():
+                # color = util.rgb_to_hex(util.rgb_blend((1, 1, 1), (1, 0, 0), address_id_to_count[node.obj_dict['name']] / address_id_count_total))
+                address_id = node.obj_dict['name']
+                if address_id in graph_nodes:
+                    count = graph_nodes[address_id]
+                    node.set_style('filled')
+                    node.set_fillcolor(address_id_to_color[address_id])
+                    node.set_color('black')
+                    node.set_fontcolor('black')
+                    color_factor = 0.75 * (math.exp(1. - count / transition_count_max) - 1.) / (math.e - 1.)
+                    # color = rgb_to_hex((color_factor, color_factor, color_factor))
+                    # node.set_color(color)
+                    node.set_penwidth(max(0.1, 4 * (1 - color_factor)))
+            for edge in graph.get_edges():
+                (left, right) = edge.obj_dict['points']
+                if (left, right) in graph_edges:
+                    count = graph_edges[(left, right)]
+                    edge.set_label('\"{:,.6f}\"'.format(count / transition_count_max))
+                    # color_factor = 1 - max(0.15, min(1, count/transition_count_max))
+                    color_factor = 0.75 * (math.exp(1. - count / transition_count_max) - 1.) / (math.e - 1.)
+                    color = util.rgb_to_hex((color_factor, color_factor, color_factor))
+                    # edge.set_color('black')
+                    edge.set_color(color)
+                    # edge.set_penwidth(max(0.1, 5 * count / transition_count_max))
+
+            graphs.append(graph)
 
         if file_name is not None:
             file_name_stats = file_name + '.txt'
@@ -194,7 +266,7 @@ class Analytics():
             file_name_addresses = file_name + '_addresses.csv'
             print('Saving addresses to {} ...'.format(file_name_addresses))
             with open(file_name_addresses, 'w') as file:
-                file.write('address_id, frequency, name, controlled, replaced, observable, observed, address_base\n')
+                file.write('address_id, frequency, name, controlled, replaced, observable, observed, {}\n'.format('address_base' if use_address_base else 'address'))
                 for key, value in address_stats.items():
                     name = '' if value[2].name is None else value[2].name
                     file.write('{}, {}, {}, {}, {}, {}, {}, {}\n'.format(value[1], value[0], name, value[2].control, value[2].replace, value[2].observable, value[2].observed, key))
@@ -237,5 +309,17 @@ class Analytics():
             if status != 0:
                 print('Could not render file {}. Check that GraphViz is installed.'.format(file_name_latent_structure_all))
 
+            for i in range(n_most_frequent):
+                trace_id = list(sorted_trace_stats.values())[i][1]
+                file_name_latent_structure = file_name + '_latent_structure_most_freq_{}_{}.dot'.format(i+1, trace_id)
+                print('Saving latent structure graph {} of {} to {} ...'.format(i+1, n_most_frequent, file_name_latent_structure))
+                with open(file_name_latent_structure, 'w') as file:
+                    file.write(graphs[i].to_string())
+
+                file_name_latent_structure_pdf = file_name + '_latent_structure_most_freq_{}_{}.pdf'.format(i+1, trace_id)
+                print('Rendering latent structure graph {} of {} to {} ...'.format(i+1, n_most_frequent, file_name_latent_structure_pdf))
+                status, result = subprocess.getstatusoutput('dot -Tpdf {} -o {}'.format(file_name_latent_structure, file_name_latent_structure_pdf))
+                if status != 0:
+                    print('Could not render file {}. Check that GraphViz is installed.'.format(file_name_latent_structure))
 
         return stats
