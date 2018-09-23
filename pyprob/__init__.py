df40c3db038cc82229ebe997ef014f5fb23cab44
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2018-09-23 15:16:53 +0100

    rebase

--- pyprob/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = '0.11.dev1'
-
-from .util import TraceMode, PriorInflation, InferenceEngine, InferenceNetwork, ObserveEmbedding, set_verbosity, set_random_seed, set_cuda
-from .state import sample, observe
-from .model import Model, ModelRemote
-from .analytics import Analytics
