a64db4491d4f47a0ff7dfbf93bda3be697937017
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2019-02-15 11:59:28 +0000

    Bump version

--- pyprob/__init__.py
+++ pyprob/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.13.0'
+__version__ = '0.13.2.dev1'
 
 from .util import TraceMode, PriorInflation, InferenceEngine, InferenceNetwork, Optimizer, ObserveEmbedding, set_verbosity, set_random_seed, set_cuda
 from .state import sample, observe, tag
