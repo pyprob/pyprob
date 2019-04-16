056cdb72cd47e68f64d404865fdc5f43c4d77491
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2019-04-16 11:18:34 +0900

    updated

--- pyprob/distributions/weibull.py
+++ pyprob/distributions/weibull.py
@@ -6,9 +6,9 @@ from .. import util
 
 class Weibull(Distribution):
     def __init__(self, scale,concentration):
-        scale = util.to_tensor(scale)
-        concentration = util.to_tensor(concentration)
-        super().__init__(name='Weibull', address_suffix='Weibull', torch_dist=torch.distributions.Weibull(scale,concentration))
+        self._scale = util.to_tensor(scale)
+        self._concentration = util.to_tensor(concentration)
+        super().__init__(name='Weibull', address_suffix='Weibull', torch_dist=torch.distributions.Weibull(self._scale,self._concentration))
 
     def __repr__(self):
         return 'Weibull(scale:{},concentration:{})'.format(self.scale, self.concentration)
@@ -18,3 +18,12 @@ class Weibull(Distribution):
 
     def icdf(self, value):
         return self._torch_dist.icdf(value)
+
+    @property
+    def scale(self):
+        return self._scale
+    
+    @property
+    def concentration(self):
+        return self._concentration
+    
