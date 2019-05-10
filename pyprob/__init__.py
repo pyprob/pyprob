dfc0874dd90ff0b0f61410c7687e02e5e7026756
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2019-05-10 23:16:04 +0100

    Decrease file_sync_timeout to 25

--- pyprob/__init__.py
+++ pyprob/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.13.2.dev13'
+__version__ = '0.13.2.dev14'
 
 from .util import TraceMode, PriorInflation, InferenceEngine, InferenceNetwork, Optimizer, LearningRateScheduler, ObserveEmbedding, set_verbosity, set_random_seed, set_device
 from .state import sample, observe, tag
