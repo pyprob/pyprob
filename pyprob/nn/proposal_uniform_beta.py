c388f006e3bdb884e187fd716c9252a640988ace
Saeid Naderiparizi <saeidnp@cs.ubc.ca>
2020-01-30 15:23:51 -0800

    ProposalUniformBeta and ProposalUniformBetaMixture issues fixed

--- pyprob/nn/proposal_uniform_beta.py
+++ pyprob/nn/proposal_uniform_beta.py
@@ -7,18 +7,23 @@ from ..distributions import Beta
 
 
 class ProposalUniformBeta(nn.Module):
-    def __init__(self, input_shape, output_shape, num_layers=2):
+    def __init__(self, input_shape, output_shape, num_layers=2, hidden_dim=None):
         super().__init__()
         input_shape = util.to_size(input_shape)
-        self._output_dim = util.prod(output_shape)
-        self._output_shape = torch.Size([-1]) + output_shape
-        self._ff = EmbeddingFeedForward(input_shape=input_shape, output_shape=torch.Size([self._output_dim * 2]), num_layers=num_layers, activation=torch.relu, activation_last=torch.relu)
+        self.output_dim = util.prod(output_shape)
+        self._ff = EmbeddingFeedForward(input_shape=input_shape,
+                                        output_shape=torch.Size([self.output_dim * 2]),
+                                        num_layers=num_layers, activation=torch.relu,
+                                        activation_last=None,
+                                        hidden_dim=hidden_dim)
         self._total_train_iterations = 0
+        self._softplus = nn.Softplus()
 
-    def forward(self, x, prior_variables):
+    def forward(self, x, prior_distribution):
+        batch_size = x.size(0)
         x = self._ff(x)
-        concentration1s = 1. + x[:, :self._output_dim].view(self._output_shape)
-        concentration0s = 1. + x[:, self._output_dim:].view(self._output_shape)
-        prior_lows = torch.stack([v.distribution.low for v in prior_variables]).view(concentration1s.size())
-        prior_highs = torch.stack([v.distribution.high for v in prior_variables]).view(concentration1s.size())
+        concentration1s = self._softplus(x[:, :self.output_dim].view(batch_size, -1))
+        concentration0s = self._softplus(x[:, self.output_dim:].view(batch_size, -1))
+        prior_lows = prior_distribution.low.view(batch_size, -1)
+        prior_highs = prior_distribution.high.view(batch_size, -1)
         return Beta(concentration1s, concentration0s, low=prior_lows, high=prior_highs)
--- pyprob/nn/proposal_uniform_beta_mixture.py
+++ pyprob/nn/proposal_uniform_beta_mixture.py
@@ -7,24 +7,36 @@ from ..distributions import Beta, Mixture
 
 
 class ProposalUniformBetaMixture(nn.Module):
-    def __init__(self, input_shape, output_shape, num_layers=2, mixture_components=10):
+    def __init__(self, input_shape, output_shape, num_layers=2, hidden_dim=None,
+                 mixture_components=10):
         super().__init__()
         # Currently only supports event_shape=torch.Size([]) for the mixture components
         self._mixture_components = mixture_components
         input_shape = util.to_size(input_shape)
-        self._ff = EmbeddingFeedForward(input_shape=input_shape, output_shape=torch.Size([3 * self._mixture_components]), num_layers=num_layers, activation=torch.relu, activation_last=None)
+        self.output_dim = util.prod(output_shape)
+        self._ff = EmbeddingFeedForward(input_shape=input_shape,
+                                        output_shape=torch.Size([(2*self.output_dim + 1) * self._mixture_components]),
+                                        num_layers=num_layers,
+                                        activation=torch.relu, activation_last=None,
+                                        hidden_dim=hidden_dim)
         self._total_train_iterations = 0
+        self._softplus = nn.Softplus()
+        self._logsoftmax = nn.LogSoftmax(dim=1)
 
-    def forward(self, x, prior_variables):
+    def forward(self, x, prior_distribution):
         batch_size = x.size(0)
         x = self._ff(x)
-        concentration1s = x[:, :self._mixture_components].view(batch_size, -1)
-        concentration0s = x[:, self._mixture_components:2*self._mixture_components].view(batch_size, -1)
-        concentration1s = 1. + torch.relu(concentration1s)
-        concentration0s = 1. + torch.relu(concentration0s)
-        coeffs = x[:, 2*self._mixture_components:].view(batch_size, -1)
-        coeffs = torch.softmax(coeffs, dim=1)
-        prior_lows = torch.stack([v.distribution.low for v in prior_variables]).view(batch_size)
-        prior_highs = torch.stack([v.distribution.high for v in prior_variables]).view(batch_size)
-        distributions = [Beta(concentration1s[:, i:i+1].view(batch_size), concentration0s[:, i:i+1].view(batch_size), low=prior_lows, high=prior_highs) for i in range(self._mixture_components)]
-        return Mixture(distributions, coeffs)
+
+        slice_size = self.output_dim*self._mixture_components
+        concentration1s = self._softplus(x[:, :slice_size].view(batch_size, -1))
+        concentration0s = self._softplus(x[:, slice_size:2*slice_size].view(batch_size, -1))
+        coeffs = x[:, 2*slice_size:].view(batch_size, -1)
+
+        prior_lows = prior_distribution.low.view(batch_size, -1)
+        prior_highs = prior_distribution.high.view(batch_size, -1)
+
+        log_coeffs = self._logsoftmax(coeffs)
+        distributions = [Beta(concentration1s[:, i*self.output_dim:(i+1)*self.output_dim].view(batch_size, self.output_dim),
+                              concentration0s[:, i*self.output_dim:(i+1)*self.output_dim].view(batch_size, self.output_dim),
+                              low=prior_lows, high=prior_highs) for i in range(self._mixture_components)]
+        return Mixture(distributions, logits=log_coeffs)
