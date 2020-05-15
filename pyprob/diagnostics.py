410262f3516119ffb7900e5911340d64415e680b
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2020-05-15 12:15:12 +0100

    fix diag for named vars without value

--- pyprob/diagnostics.py
+++ pyprob/diagnostics.py
@@ -592,20 +592,22 @@ def _variable_values(trace_dist, names=None, n_most_frequent=None, num_traces=No
     # Select named variables to process
     for name in names:
         variable = trace_dist[0].named_variables[name]
-        if variable.value is not None and variable.value.nelement() == 1:
-            if variable.address not in variable_values:
-                variable_values[variable.address] = {'variable': None, 'values': np.ones(num_traces) * np.nan}
-            variable_values[variable.address]['variable'] = variable
+        if variable.value is not None:
+            if variable.value.nelement() == 1:
+                if variable.address not in variable_values:
+                    variable_values[variable.address] = {'variable': None, 'values': np.ones(num_traces) * np.nan}
+                variable_values[variable.address]['variable'] = variable
 
     # Select most frequent variables to process (either named or not named)
     if n_most_frequent is not None:
         addresses = _n_most_frequent_addresses(trace_dist, n_most_frequent, num_traces)
         for address in addresses:
             variable = trace_dist[0].variables_dict_address[address]
-            if variable.value is not None and variable.value.nelement() == 1:
-                if variable.address not in variable_values:
-                    variable_values[variable.address] = {'variable': None, 'values': np.ones(num_traces) * np.nan}
-                variable_values[variable.address]['variable'] = variable
+            if variable.value is not None:
+                if variable.value.nelement() == 1:
+                    if variable.address not in variable_values:
+                        variable_values[variable.address] = {'variable': None, 'values': np.ones(num_traces) * np.nan}
+                    variable_values[variable.address]['variable'] = variable
 
     if len(variable_values) == 0:
         raise RuntimeError('No variables with scalar value.')
