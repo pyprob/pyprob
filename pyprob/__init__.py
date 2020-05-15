64d822acf8bca1771ae2d75f9a6fa1a0adfe1861
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2020-05-15 11:38:25 +0100

    fix diag for named vars without value

--- pyprob/__init__.py
+++ pyprob/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 
 from .util import TraceMode, PriorInflation, InferenceEngine, InferenceNetwork, ImportanceWeighting, Optimizer, LearningRateScheduler, ObserveEmbedding, set_verbosity, set_device, seed
 from .state import sample, observe, tag
