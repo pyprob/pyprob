a8b03eed4d9b779b05396e4f951184906123f4c1
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2021-05-29 19:20:32 +0100

    docs tweak

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,3 +49,9 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = '_static/pyprob-logo-500px.png'
+html_favicon = '_static/favicon.ico'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': True
+}
