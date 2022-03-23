222a122bd7491c3d2db1f0c2721f99a6e4aefa9d
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2022-03-23 21:45:37 +0000

    version

--- pyprob/__init__.py
+++ pyprob/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.4.2'
+__version__ = '1.5.0'
 
 from .util import TraceMode, PriorInflation, InferenceEngine, InferenceNetwork, Optimizer, LearningRateScheduler, ObserveEmbedding, set_verbosity, set_device, seed
 from .state import sample, observe, factor, tag
